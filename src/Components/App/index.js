d44dc6c1611ba660d8c6928a5017852d00d06d27
Ropenfold <stuartroper@gmail.com>
2019-03-18 20:52:24 +0000

    stu changes at home

---
+++ src/Components/App/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    quotes: []
+  }
+  
+    componentDidMount() {
+      fetch(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      )
+        .then(res => res.json())
+        .then(data => this.setState({quote: data }));
+        console.log(this.state.quotes);
+    }
+
+  render() {
+    return (
+      <ul>
+        
+      </ul>
+    )
+  }
+}
+
+export default App;
